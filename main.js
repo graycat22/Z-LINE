576f75b703d52b5fd7de78d53f2f0f4c6769d765
remi.yumeshima <asaneko22@gmail.com>
2023-02-17 20:41:07 +0000

    first commit

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ function doPost(e) {
   var url = 'https://api.line.me/v2/bot/message/reply';
   var channelToken = 'JM3WilyttcFJpQlQQqgjlBEg4Q30hzhDPiQgq95Upy+obYmenxcwcePqNRMc8a6p1uN7h/8Im8uOpfNwg/Y2fSVlajDtY0Dend4gWBR3GcKhEJs6VNV/5eo6NmyxpFyiqPiY/g6Esfw65/klAxGaGAdB04t89/1O/w1cDnyilFU=';
 
+  var message = JSON.parse(e.postData.contents).events[0].message;
+
   var messages = [{
     'type': 'text',
-    'text': `${JSON.parse(e.postData.contents).events[0].message}`,
+    'text': message.text,
   }];
 
   UrlFetchApp.fetch(url, {
